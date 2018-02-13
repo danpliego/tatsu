90d590a7f45d0d242f875e64e617d1eb54e3459d
Daniel Pliego <danielpliego@gmail.com>
2018-02-12 21:54:07 -0600

    first commit

---
+++ base.php
@@ -0,0 +1,38 @@
+<?php
+
+use Roots\Sage\Setup;
+use Roots\Sage\Wrapper;
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <style>
+      html {
+        margin-top: 0!important;
+      }
+    </style>
+    <div class="wrap container-fluid" role="document">
+      <div class="content row">
+        <div class="sidebar">
+          <div class="sidebar-content">
+            <?php
+              do_action('get_header');
+              get_template_part('templates/header');
+            ?>
+            <?php if (Setup\display_sidebar()) : ?>
+              <?php include Wrapper\sidebar_path(); ?>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="main">
+          <div class="main-content">
+            <?php include Wrapper\template_path(); ?>
+          </div>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<header class="banner">
+  <div class="container text-center">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/images/logo.png" alt="" width="210"></a>
+    <h6>
+      Artist
+      <span class="tatsu-red">/</span>
+      Illustrator
+      <span class="tatsu-red">/</span>
+      Art Director
+    </h6>
+    <div class="menu-dots"></div>
+    <nav class="nav-primary">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
+    <ul class="social-menu list-unstyled">
+      <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/images/instagram.png" alt="" width="24"></a></li>
+      <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/images/facebook.png" alt="" width="20"></a></li>
+    </ul>
+  </div>
+</header>
